c6aeb3585bcac5711510253a65cb99f0414b286b
LionBoy19 <101229795+LionBoy19@users.noreply.github.com>
2022-03-19 16:29:43 -0600

    Add files via upload

---
+++ src/Derived1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Randy on 19/3/2022.
+//
+
+#include "Derived1.h"
+#include <iostream>
+using namespace std;
+
+Derived1::Derived1() {}
+
+Derived1::~Derived1() {}
+
+void Derived1::do_something() {
+    cout<<"Derived1!!!"<<endl;
+}
---
+++ src/Derived2.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Randy on 19/3/2022.
+//
+
+#include "Derived2.h"
+#include <iostream>
+using namespace std;
+
+Derived2::Derived2() {}
+
+Derived2::~Derived2() {}
+
+void Derived2::do_something() {
+    cout<<"Derived2!!!"<<endl;
+}
